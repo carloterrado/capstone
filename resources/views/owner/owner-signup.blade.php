fb00d1a80ae6ae6bcc7b0738a2347be7e2672c37
carloterrado <iamterradocarlo@gmail.com>
2023-02-27 12:49:58 +0800

    test htaccess

--- resources/views/owner/owner-signup.blade.php
+++ resources/views/owner/owner-signup.blade.php
@@ -77,6 +77,7 @@
                           <label id="owner-signup-email-error" for="owner-signup-email" class="pointer-events-none absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-gray-900 peer-focus:font-semibold peer-placeholder-shown:scale-100 
                           peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Email</label>
                         </div>
+                        
                          
                         <!-- Birthdate & contact num -->
                         <div class="sm:flex justify-between">
