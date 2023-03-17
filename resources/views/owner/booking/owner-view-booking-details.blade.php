0bc4c615a3ab3b1cb53234f2ff7b9d87f5f0cc5d
carloterrado <iamterradocarlo@gmail.com>
2023-03-17 22:59:14 +0800

    .

--- resources/views/owner/booking/owner-view-booking-details.blade.php
+++ resources/views/owner/booking/owner-view-booking-details.blade.php
@@ -177,12 +177,14 @@
                     <div class="col-span-8 md:col-span-3">
                         <div class="bg-[#f5f5f5] pb-4">
                             <div class="p-2 md:p-6 md:pb-4 sm:pt-0">
+                                @if ($book['status'] === 'pending')
                                 <div class="flex " >   
                                     <h3 class="col-span-3 text-sm  font-semibold">View Schedule </h3>
                                    
                                     <svg class="cursor-pointer view-car-schedule" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path fill="#e84949" d="M21 20V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2zM9 18H7v-2h2v2zm0-4H7v-2h2v2zm4 4h-2v-2h2v2zm0-4h-2v-2h2v2zm4 4h-2v-2h2v2zm0-4h-2v-2h2v2zm2-5H5V7h14v2z"/></svg>
                                   
                                 </div> 
+                                @endif
                                 <div class="">   
                                     <h3 class="col-span-3 text-sm font-semibold">Destination:</h3>
                                     <p class="col-span-3 text-sm font-semibold">{{$book['booking_info']['destination']}}</p>
